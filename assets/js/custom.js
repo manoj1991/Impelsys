b00364b94c7b9f806a46a8d1c8433ad61ea032e2
Manoj Kadolkar(Associate) <M9010216@mindtree.com>
2018-08-28 12:55:53 +0530

    Updated version

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+(function($) {
+    $(function() {
+        $('.h5p-container').h5p({
+            frameJs: 'assets/js/h5p-standalone-frame.min.js',
+            frameCss: 'assets/styles/h5p.css',
+            h5pContent: 'workspace'
+        });
+    });
+})(H5P.jQuery);
